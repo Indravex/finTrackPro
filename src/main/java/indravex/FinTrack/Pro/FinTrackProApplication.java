5d111c3dea3acbaea0fcec03882a0b3badfcb1d9
Indravex <indravexgithub@gmail.com>
2026-08-31 12:16:24 +0530

    Initial commit

---
+++ src/main/java/indravex/FinTrack/Pro/FinTrackProApplication.java
@@ -0,0 +1,13 @@
+package indravex.FinTrack.Pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinTrackProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinTrackProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/indravex/FinTrack/Pro/FinTrackProApplicationTests.java
@@ -0,0 +1,13 @@
+package indravex.FinTrack.Pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinTrackProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

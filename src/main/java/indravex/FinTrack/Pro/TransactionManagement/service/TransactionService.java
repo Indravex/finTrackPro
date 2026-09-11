58b27ce886f42830f2368756db7cae3dcb3ca669
Kshitija Awad <ksitijaawad974@gmail.com>
2026-09-11 17:45:32 +0530

    Add transaction filter API

--- src/main/java/indravex/FinTrack/Pro/TransactionManagement/service/TransactionService.java
+++ src/main/java/indravex/FinTrack/Pro/TransactionManagement/service/TransactionService.java
@@ -3,7 +3,17 @@ package indravex.FinTrack.Pro.TransactionManagement.service;
 import indravex.FinTrack.Pro.TransactionManagement.dto.TransactionRequest;
 import indravex.FinTrack.Pro.TransactionManagement.entity.Transaction;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface TransactionService {
 
     Transaction createTransaction(TransactionRequest request);
+
+    List<Transaction> getTransactions(
+            Long companyId,
+            Long accountId,
+            LocalDate fromDate,
+            LocalDate toDate
+    );
 }
